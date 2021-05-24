cfa029eae1a0c6f2d5ce977c4b9f23f869877a1a
P3rni1 <nicolasacostap@gmail.com>
2021-05-24 17:57:34 +0200

    Model change 🗃️

--- src/model/question.js
+++ src/model/question.js
@@ -5,6 +5,9 @@ const questionSchema = new mongoose.Schema(
     name: {
       type: String,
     },
+    info: {
+      type: String,
+    },
     question: {
       type: String,
       required: true,
